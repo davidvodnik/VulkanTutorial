52831242057d20b6829ad3df21ce259ab9d0ec0a
David Vodnik <davidvodnik@gmail.com>
2018-08-08 21:35:55 +0200

    NEW: instance

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -26,7 +27,41 @@ private:
 	}
 
 	void InitVulkan() {
+		CreateInstance();
+	}
 
+	void CreateInstance() {
+		VkApplicationInfo app_info = {};
+		app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		app_info.pApplicationName = "Vulkan Tutorial Triangle";
+		app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		app_info.pEngineName = "No Engine";
+		app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		app_info.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo create_info = {};
+		create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		create_info.pApplicationInfo = &app_info;
+
+		uint32_t glfw_extension_count = 0;
+		const char** glfw_extensions;
+		glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+		create_info.enabledExtensionCount = glfw_extension_count;
+		create_info.ppEnabledExtensionNames = glfw_extensions;
+
+		create_info.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
+
+		// print available extensions
+		uint32_t extension_count = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+		std::vector<VkExtensionProperties> extensions(extension_count);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extensions.data());
+		std::cout << "available extensions:" << std::endl;
+		for (const auto& extension : extensions) {
+			std::cout << "\t" << extension.extensionName << std::endl;
+		}
 	}
 
 	void MainLoop() {
@@ -36,12 +71,15 @@ private:
 	}
 
 	void Cleanup() {
+		vkDestroyInstance(instance, nullptr);
+		
 		glfwDestroyWindow(window);
 
 		glfwTerminate();
 	}
 
 	GLFWwindow* window;
+	VkInstance instance;
 };
 
 int main() {
