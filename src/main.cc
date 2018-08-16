1cd95ee1e8fb9b83d256025def20d61b18b035d1
David Vodnik <davidvodnik@gmail.com>
2018-08-16 22:12:41 +0200

    NEW: image views

--- src/main.cc
+++ src/main.cc
@@ -132,6 +132,7 @@ private:
 		PickPhysicalDevice();
 		CreateLogicalDevice();
 		CreateSwapChain();
+		CreateImageViews();
 	}
 
 	void CreateInstance() {
@@ -446,6 +447,30 @@ return VK_FALSE;
 		swap_chain_extent = extent;
 	}
 
+	void CreateImageViews() {
+		swap_chain_image_views.resize(swap_chain_images.size());
+		for (size_t i = 0; i < swap_chain_image_views.size(); ++i) {
+			VkImageViewCreateInfo create_info = {};
+			create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			create_info.image = swap_chain_images[0];
+			create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			create_info.format = swap_chain_image_format;
+			create_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			create_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			create_info.subresourceRange.baseMipLevel = 0;
+			create_info.subresourceRange.levelCount = 1;
+			create_info.subresourceRange.baseArrayLayer = 0;
+			create_info.subresourceRange.layerCount = 1;
+
+			if (vkCreateImageView(device, &create_info, nullptr, &swap_chain_image_views[i]) != VK_SUCCESS) {
+				assert(0);
+			}
+		}
+	}
+
 	void MainLoop() {
 		while (!glfwWindowShouldClose(window)) {
 			glfwPollEvents();
@@ -453,6 +478,9 @@ return VK_FALSE;
 	}
 
 	void Cleanup() {
+		for (auto image_view : swap_chain_image_views) {
+			vkDestroyImageView(device, image_view, nullptr);
+		}
 		vkDestroySwapchainKHR(device, swap_chain, nullptr);
 
 		vkDestroyDevice(device, nullptr);
@@ -482,6 +510,7 @@ return VK_FALSE;
 	std::vector<VkImage> swap_chain_images;
 	VkFormat swap_chain_image_format;
 	VkExtent2D swap_chain_extent;
+	std::vector<VkImageView> swap_chain_image_views;
 };
 
 int main() {
