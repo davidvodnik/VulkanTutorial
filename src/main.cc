2f98ad6c6be9798e296866217779a098f496eb9f
David Vodnik <davidvodnik@gmail.com>
2018-08-18 00:37:59 +0200

    NEW: Render passes

--- src/main.cc
+++ src/main.cc
@@ -152,6 +152,7 @@ private:
 		CreateLogicalDevice();
 		CreateSwapChain();
 		CreateImageViews();
+		CreateRenderPass();
 		CreateGraphicsPipeline();
 	}
 
@@ -491,6 +492,38 @@ return VK_FALSE;
 		}
 	}
 
+	void CreateRenderPass() {
+		VkAttachmentDescription color_attachment = {};
+		color_attachment.format = swap_chain_image_format;
+		color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+		color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+		color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+		VkAttachmentReference color_attachment_ref = {};
+		color_attachment_ref.attachment = 0;
+		color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+		VkSubpassDescription subpass = {};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &color_attachment_ref;
+
+		VkRenderPassCreateInfo render_pass_info = {};
+		render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		render_pass_info.attachmentCount = 1;
+		render_pass_info.pAttachments = &color_attachment;
+		render_pass_info.subpassCount = 1;
+		render_pass_info.pSubpasses = &subpass;
+
+		if (vkCreateRenderPass(device, &render_pass_info, nullptr, &render_pass) != VK_SUCCESS) {
+			assert(0);
+		}
+	}
+
 	void CreateGraphicsPipeline() {
 		auto vert_shader_code = ReadFile("shaders/vert.spv");
 		auto frag_shader_code = ReadFile("shaders/frag.spv");
@@ -650,6 +683,8 @@ return VK_FALSE;
 	void Cleanup() {
 		vkDestroyPipelineLayout(device, pipeline_layout, nullptr);
 
+		vkDestroyRenderPass(device, render_pass, nullptr);
+
 		for (auto image_view : swap_chain_image_views) {
 			vkDestroyImageView(device, image_view, nullptr);
 		}
@@ -683,6 +718,7 @@ return VK_FALSE;
 	VkFormat swap_chain_image_format;
 	VkExtent2D swap_chain_extent;
 	std::vector<VkImageView> swap_chain_image_views;
+	VkRenderPass render_pass;
 	VkPipelineLayout pipeline_layout;
 };
 
