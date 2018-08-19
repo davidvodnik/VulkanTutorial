a70ba2f862a925b7a589c07c4cd9ff58ba88b20d
David Vodnik <davidvodnik@gmail.com>
2018-08-19 10:35:55 +0200

    NEW: rendering and presentation

--- src/main.cc
+++ src/main.cc
@@ -11,6 +11,8 @@
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
+const int MAX_FRAMES_IN_FLIGHT = 2;
+
 const std::vector<const char*> validation_layers = {
 	"VK_LAYER_LUNARG_standard_validation"
 };
@@ -796,12 +798,23 @@ return VK_FALSE;
 	}
 
 	void CreateSemaphores() {
+		image_available_semaphores.resize(MAX_FRAMES_IN_FLIGHT);
+		render_finished_semaphores.resize(MAX_FRAMES_IN_FLIGHT);
+		in_flight_fences.resize(MAX_FRAMES_IN_FLIGHT);
+
 		VkSemaphoreCreateInfo semaphore_info = {};
 		semaphore_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
 
-		if (vkCreateSemaphore(device, &semaphore_info, nullptr, &image_available_semaphore) != VK_SUCCESS ||
-			vkCreateSemaphore(device, &semaphore_info, nullptr, &render_finished_semaphore) != VK_SUCCESS) {
-			assert(0);
+		VkFenceCreateInfo fence_info = {};
+		fence_info.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+		fence_info.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+		for (int i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
+			if (vkCreateSemaphore(device, &semaphore_info, nullptr, &image_available_semaphores[i]) != VK_SUCCESS ||
+				vkCreateSemaphore(device, &semaphore_info, nullptr, &render_finished_semaphores[i]) != VK_SUCCESS ||
+				vkCreateFence(device, &fence_info, nullptr, &in_flight_fences[i]) != VK_SUCCESS) {
+				assert(0);
+			}
 		}
 	}
 
@@ -815,13 +828,16 @@ return VK_FALSE;
 	}
 
 	void DrawFrame() {
+		vkWaitForFences(device, 1, &in_flight_fences[current_frame], VK_TRUE, std::numeric_limits<uint64_t>::max());
+		vkResetFences(device, 1, &in_flight_fences[current_frame]);
+
 		uint32_t image_index;
-		vkAcquireNextImageKHR(device, swap_chain, std::numeric_limits<uint64_t>::max(), image_available_semaphore, VK_NULL_HANDLE, &image_index);
+		vkAcquireNextImageKHR(device, swap_chain, std::numeric_limits<uint64_t>::max(), image_available_semaphores[current_frame], VK_NULL_HANDLE, &image_index);
 
 		VkSubmitInfo submit_info = {};
 		submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
 
-		VkSemaphore wait_semaphores[] = { image_available_semaphore };
+		VkSemaphore wait_semaphores[] = { image_available_semaphores[current_frame] };
 		VkPipelineStageFlags wait_stages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
 		submit_info.waitSemaphoreCount = 1;
 		submit_info.pWaitSemaphores = wait_semaphores;
@@ -829,11 +845,11 @@ return VK_FALSE;
 		submit_info.commandBufferCount = 1;
 		submit_info.pCommandBuffers = &command_buffers[image_index];
 
-		VkSemaphore signal_semaphores[] = { render_finished_semaphore };
+		VkSemaphore signal_semaphores[] = { render_finished_semaphores[current_frame] };
 		submit_info.signalSemaphoreCount = 1;
 		submit_info.pSignalSemaphores = signal_semaphores;
 
-		if (vkQueueSubmit(graphics_queue, 1, &submit_info, VK_NULL_HANDLE) != VK_SUCCESS) {
+		if (vkQueueSubmit(graphics_queue, 1, &submit_info, in_flight_fences[current_frame]) != VK_SUCCESS) {
 			assert(0);
 		}
 
@@ -850,13 +866,15 @@ return VK_FALSE;
 
 		vkQueuePresentKHR(present_queue, &present_info);
 
-		vkQueueWaitIdle(present_queue);
+		current_frame = (current_frame + 1) % MAX_FRAMES_IN_FLIGHT;
 	}
 
 	void Cleanup() {
-		vkDestroySemaphore(device, render_finished_semaphore, nullptr);
-
-		vkDestroySemaphore(device, image_available_semaphore, nullptr);
+		for (int i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
+			vkDestroySemaphore(device, render_finished_semaphores[i], nullptr);
+			vkDestroySemaphore(device, image_available_semaphores[i], nullptr);
+			vkDestroyFence(device, in_flight_fences[i], nullptr);
+		}
 
 		vkDestroyCommandPool(device, command_pool, nullptr);
 
@@ -909,8 +927,10 @@ return VK_FALSE;
 	std::vector<VkFramebuffer> swap_chain_framebuffers;
 	VkCommandPool command_pool;
 	std::vector<VkCommandBuffer> command_buffers;
-	VkSemaphore image_available_semaphore;
-	VkSemaphore render_finished_semaphore;
+	std::vector<VkSemaphore> image_available_semaphores;
+	std::vector<VkSemaphore> render_finished_semaphores;
+	std::vector<VkFence> in_flight_fences;
+	size_t current_frame = 0;
 };
 
 int main() {
